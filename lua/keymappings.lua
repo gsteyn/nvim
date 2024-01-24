6961b0a59fc9d7663c4462eb52a8dbd32bb4b492
Gareth Steyn <steyn.gareth@gmail.com>
2024-01-24 15:30:04 +0100

    initial set up

---
+++ lua/keymappings.lua
@@ -0,0 +1,7 @@
+-- key bindings
+vim.g.mapleader = " "
+
+vim.keymap.set("n", "<leader>s", ":w<CR>")
+vim.keymap.set("n", "<leader>q", ":wq<CR>")
+vim.keymap.set("n", "<leader>qa", ":wqa<CR>")
+
---
+++ lua/options.lua
@@ -0,0 +1,8 @@
+-- options
+vim.opt.expandtab=true
+vim.opt.tabstop=4
+vim.opt.softtabstop=4
+vim.opt.shiftwidth=4
+vim.opt.number=true
+vim.opt.relativenumber=true
+
