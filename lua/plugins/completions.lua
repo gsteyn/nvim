17a0e72dcec1f4287f1ed6b7fe81328a49d29e0f
Gareth Steyn <steyn.gareth@gmail.com>
2024-01-25 15:24:20 +0100

    refactor telescope and cmp plugin setups

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,22 +1,24 @@
 return {
 	{
-		"hrsh7th/cmp-nvim-lsp",
-	},
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
-		},
-	},
-	{
 		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp", -- source from lsp
+			"hrsh7th/cmp-buffer", -- source from buffer
+			"hrsh7th/cmp-path", -- source from file system path
+			"L3MON4D3/LuaSnip", -- snippet engine
+			"saadparwaiz1/cmp_luasnip", -- for autocompletion
+			"rafamadriz/friendly-snippets", -- useful snippets
+		},
 		config = function()
 			-- Set up nvim-cmp.
 			local cmp = require("cmp")
 			require("luasnip.loaders.from_vscode").lazy_load()
 
 			cmp.setup({
+                completion = {
+                    completeopt = "menu,menuone,preview,noselect",
+                },
 				snippet = {
 					-- REQUIRED - you must specify a snippet engine
 					expand = function(args)
@@ -31,12 +33,13 @@ return {
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
+					["<C-a>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
 					{ name = "luasnip" }, -- For luasnip users.
+                    { name = "path" }
 				}, {
 					{ name = "buffer" },
 				}),
