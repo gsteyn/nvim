38b6232858cfbf4ad9b14d4e362d01f35357f0a9
Gareth Steyn <steyn.gareth@gmail.com>
2024-01-30 11:13:49 +0100

    add prettierd and eslint_d to none-ls config

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,14 +1,39 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
+    "nvimtools/none-ls.nvim",
+    config = function()
+        local null_ls = require("null-ls")
 
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-			},
-		})
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.stylua.with({
+                    filetypes = { "lua", "luau" },
+                }),
+                null_ls.builtins.formatting.prettierd.with({
+                    filetypes = {
+                        "javascript",
+                        "javascriptreact",
+                        "typescript",
+                        "typescriptreact",
+                        "vue",
+                        "css",
+                        "scss",
+                        "less",
+                        "html",
+                        "json",
+                        "jsonc",
+                        "yaml",
+                        "markdown",
+                        "markdown.mdx",
+                        "graphql",
+                        "handlebars",
+                    },
+                }),
+                null_ls.builtins.diagnostics.eslint_d.with({
+                    filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" },
+                }),
+            },
+        })
 
-		vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, {})
-	end,
+        vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, {})
+    end,
 }
