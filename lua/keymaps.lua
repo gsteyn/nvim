23600cb211b5743b05624652157f51648692be8e
Gareth Steyn <steyn.gareth@gmail.com>
2024-02-21 21:31:32 +0100

    add keymap to go back to previous buffer

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,7 +5,11 @@ vim.keymap.set("n", "<leader>s", ":w<CR>")
 vim.keymap.set("n", "<leader>q", ":wq<CR>")
 vim.keymap.set("n", "<leader>qa", ":wqa<CR>")
 
+-- move selected line up/down and auto-fix indentation
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("i", "<C-c>", "<Esc>")
+
+-- go back to previous buffer
+vim.keymap.set("n", "<leader>b", "<cmd>b#<cr>")
