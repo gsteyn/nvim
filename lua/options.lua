b87484cb5eb777d99b07ac0a6af67bd12d53dd8f
Gareth Steyn <steyn.gareth@gmail.com>
2024-02-21 21:18:07 +0100

    add extra options

--- lua/options.lua
+++ lua/options.lua
@@ -1,8 +1,13 @@
--- options
+-- indentation
 vim.opt.expandtab=true
 vim.opt.tabstop=4
 vim.opt.softtabstop=4
 vim.opt.shiftwidth=4
+-- line number
 vim.opt.number=true
 vim.opt.relativenumber=true
-
+-- line wrap
+vim.opt.wrap=false
+vim.opt.colorcolumn="80"
+-- scroll & cursor location
+vim.opt.scrolloff=8
