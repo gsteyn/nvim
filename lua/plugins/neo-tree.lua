204b729738b40ac00a354e34678e492cc47b5ddb
gareth <gareth.steyn@solvimon.com>
2024-05-10 11:54:51 +0200

    show and search hidden files by default

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,8 +6,23 @@ return {
         "nvim-tree/nvim-web-devicons",
         "MunifTanjim/nui.nvim",
     },
+    opts = {
+        filesystem = {
+            filtered_items = {
+                visible = true,
+                show_hidden_count = true,
+                hide_dotfiles = false,
+                hide_gitignored = true,
+                hide_by_name = {
+                    -- '.git',
+                    -- '.DS_Store',
+                    -- 'thumbs.db',
+                },
+                never_show = {},
+            },
+        }
+    },
     config = function()
         vim.keymap.set("n", "<leader>ee", ":Neotree toggle<CR>")
     end
 }
-
