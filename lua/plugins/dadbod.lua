d80e00225e4f80c66df39687ecec17b1d71278df
gareth <gareth.steyn@solvimon.com>
2024-08-26 14:58:13 +0200

    set dadbod-ui to not fold results by default

--- lua/plugins/dadbod.lua
+++ lua/plugins/dadbod.lua
@@ -5,6 +5,8 @@ return {
         "kristijanhusak/vim-dadbod-completion",
     },
     config = function()
+        vim.opt.foldenable = false
+
         local keymap = vim.keymap
         keymap.set("n", "<leader>db", "<cmd>DBUIToggle<cr>", { desc = "Open up DB UI" })
     end,
