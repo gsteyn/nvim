dbf3c89acddc8c0c1c3289da745c0eb9c5be04f1
Gareth Steyn <gareth.steyn@camunda.com>
2025-03-24 13:18:57 +0100

    change theme to catppuccin latte

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -4,6 +4,7 @@ return {
     name = "catppuccin",
     priority = 1000,
     config = function()
-        vim.cmd.colorscheme("catppuccin-macchiato")
+        vim.cmd.colorscheme("catppuccin-latte")
+       -- vim.cmd.colorscheme("catppuccin-macchiato")
     end,
 }
